1ee7b47efe7911becf60d16bbbf8079e562428e0
Baljeet Singh <baljeet.tech01@gmail.com>
2022-01-12 15:20:22 +0530

    assignment 2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -1,10 +1,25 @@
 /* 1. Do the following
 
 1. Declare a variable `user` and take the name using `prompt`. Display the value stored in `user` using `alert`.
+
+
+
+
 2. Now copy the value of `user` into a new variable named `userName`. Log the value of both `user` and `userName` using `console.log`.
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
+
+let user = prompt("userName")
+alert(`${user}`);
+
+let userName = Baljeet;
+console.log(user, userName);
+
+
+
+
+
 
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
@@ -14,7 +29,9 @@ Use `prompt` to take two numbers from user and store it in variable `numA` and `
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++)
+    if (j % 2 === 0)
+     console.log(j);
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
